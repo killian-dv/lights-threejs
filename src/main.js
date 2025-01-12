00fd13d84cb3f8150bb0e7595e0cc26fc7c38ec0
killian-dv <killian.david@etudiant.univ-reims.fr>
2025-01-12 19:05:19 +0100

    feat: add multiple types of lights

--- src/main.js
+++ src/main.js
@@ -19,12 +19,38 @@ const scene = new THREE.Scene();
  */
 const ambientLight = new THREE.AmbientLight(0xffffff, 1.5);
 scene.add(ambientLight);
+gui.add(ambientLight, "intensity").min(0).max(3).step(0.001);
 
-const pointLight = new THREE.PointLight(0xffffff, 50);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
+const directionalLight = new THREE.DirectionalLight(0xff0000, 1.5);
+directionalLight.position.set(1, 1, 0);
+scene.add(directionalLight);
+
+const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 1.5);
+scene.add(hemisphereLight);
+
+const pointLight = new THREE.PointLight(0xff9000, 1.5, 10, 2);
+pointLight.position.set(1, -0.5, 1);
 scene.add(pointLight);
+
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 5, 1, 1);
+rectAreaLight.position.set(-1.5, 0, 1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+
+const spotLight = new THREE.SpotLight(
+  0x78ff00,
+  4.5,
+  10,
+  Math.PI * 0.1,
+  0.25,
+  1
+);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
+
+// add target to scene
+spotLight.target.position.x = -0.75;
+scene.add(spotLight.target);
 
 /**
  * Objects
